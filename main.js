cac4eae56ef05c7fdcb650726c71099e508f6e62
Steve122312312 <108618719+Steve122312312@users.noreply.github.com>
2023-04-08 15:41:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+    song = loadSound("music.mp3")
+    song2 = loadSound("music2.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600, 500)
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
